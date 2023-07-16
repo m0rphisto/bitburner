4e50ca2ba05482f044a89c6bd563487f0f5cc1b3
m0rphisto <warpfactor.net@gmail.com>
2023-07-16 03:25:29 +0200

    We need to get the r00ted server working again...

--- scripts/restart.js
+++ scripts/restart.js
@@ -1,9 +1,9 @@
 /** 
  * filename: restart.js
  *     date: 2023-07-15
- *  version: 0.2
+ *  version: 0.3
  *   author: .m0rph
- *      RAM: 2.90GB
+ *      RAM: 7.00GB
  * 
  * description:
  *    In case of an infinite loop, respectively game crash or augment installation (soft reset)
@@ -15,35 +15,80 @@
 
 import {c} from '/modules/colors.js';
 import {d} from '/modules/datetime.js';
+import {a} from '/modules/arguments.js';
 
 export async function main(ns) {
 
-   // Greetings new run ...
-   ns.tprintf(`${c.yellow}restart run on ${d.getdate()} at ${d.gettime()}${c.reset}`);
+   const mns = {
 
-   //we start with the hosts on our own subnet.
-   let target, hackit = '/scripts/hackit.js', net = ns.scan('home');
+      /**
+       * Property: Hosts, that were already root for subnet scanning. 
+       */
+      scanned: ['home'],
 
 
-   while (target = net.shift()) {
+      /**
+       * Method: Validate passed argument.
+       */
+      check: (arg) => {
+         return a.str(arg) ? ns.serverExists(arg) ? arg : mns.exit('Target does not exist.') : undefined;
+      },
 
-      ns.tprintf(`${c.yellow}Exploiting ${target} ...${c.reset}`);
+      /**
+       * Method: Scan the actual subnet for hosts and remember root.
+       */
+      scan: (hostname) => {
+         let host = (hostname) ? hostname : ns.getHostname();
+         if (! mns.scanned.includes(host)) mns.scanned.push(host);
+         return ns.scan(host);
+      },
 
-      const ports = ns.getServerNumPortsRequired(target); // Open ports required to successfully NUKE the target.
+      /**
+       * Method: Get all servers and restart hackit on them.
+       */
+      run: (target) => {
 
-      if (ports > 0) {
-         // Do we have to open ports?
-         if (ns.fileExists('BruteSSH.exe',  'home')) { ns.brutessh(target)  }
-         if (ns.fileExists('FTPCrack.exe',  'home')) { ns.ftpcrack(target)  }
-         if (ns.fileExists('HTTPWorm.exe',  'home')) { ns.httpworm(target)  }
-         if (ns.fileExists('relaySMTP.exe', 'home')) { ns.relaysmtp(target) }
-         if (ns.fileExists('SQLInject.exe', 'home')) { ns.sqlinject(target) }
+         if (! target) mns.exit('ERROR ARGS :: String expected.')
+
+         // Greetings new run ...
+         ns.tprintf(`${c.yellow}restart run on ${d.getdate()} at ${d.gettime()}${c.reset}`);
+
+         //we start with the hosts on our own subnet.
+         const hackit    = '/scripts/hackit.js';
+         const scriptram = ns.getScriptRam(hackit);
+         let host, hosts = ns.scan('home');
+
+         while (host = hosts.shift()) {
+
+            if (mns.scanned.includes(host)) continue;
+            
+            let h = ns.getServer(host);
+
+            if (! h.purchasedByPlayer) hosts = hosts.concat(mns.scan(h.hostname));
+            if (  h.purchasedByPlayer || h.backdoorInstalled) {
+
+               const max = ns.getServerMaxRam(h.hostname); 
+
+               if (max > 0) {
+
+                  const threads = Math.floor(max / scriptram); 
+
+                  ns.scp(hackit, h.hostname);
+                  ns.exec(hackit, h.hostname, threads, target);
+                  ns.tprintf(`${c.green}SUCCESS :: hackit is running on ${h.hostname} and hacking ${target}${c.reset}`);
+               }
+            }
+         }
+      },
+
+      /**
+       * Method: Error handler.
+       */
+      exit: (msg) => {
+         ns.tprintf(`${c.red}${msg} Exiting !!!${c.reset}`);
+         ns.exit();
       }
+   };
 
-      ns.scp(hackit, target);
-      ns.nuke(target);
-
-      ns.tprint(`${c.green}\t${target} nuked and hackit.js copied.${c.reset}`);
-   }
-   ns.tprint(`${c.green}Don't forget the backdoors. Exiting !!!${c.reset}`);
+   mns.run(a.count(ns.args, 1) ? mns.check(ns.args[0]) : mns.exit('No target passed'));
 }
