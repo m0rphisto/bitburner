ba1e12383e4be5391bf283dea3ef359b545ae7e8
m0rphisto <warpfactor.net@gmail.com>
2023-07-17 09:22:29 +0200

    Renamed restart to reboot and added a new one.

--- scripts/reboot.js
+++ scripts/reboot.js
@@ -1,12 +1,13 @@
 /** 
- * filename: restart.js
- *     date: 2023-07-17
- *  version: 0.1
+ * filename: reboot.js
+ *     date: 2023-07-16
+ *  version: 0.4
  *   author: .m0rph
- *      RAM: 7.80GB
+ *      RAM: 7.00GB
  * 
  * description:
- *    In case of new analysis, sometimes we need to restart hackit with another target !!!
+ *    In case of an infinite loop, respectively game crash or augment installation (soft reset)
+ *    we need to restart hackit automatically !!!
  * 
  * @param {NS} ns
  *             ns.args[0]  string   Hostname of target server
@@ -36,6 +37,30 @@ export async function main(ns) {
       },
 
       /**
+       * Method: No backdoor? No admin rights? OK, let's get'em.
+       */
+      exploit: (h) => {
+
+         if (h.numOpenPortsRequired > 0) {
+            
+            // Do we have to open ports?
+            if (ns.fileExists('BruteSSH.exe',  'home')) { ns.brutessh(h.hostname);  }
+            if (ns.fileExists('FTPCrack.exe',  'home')) { ns.ftpcrack(h.hostname);  }
+            if (ns.fileExists('HTTPWorm.exe',  'home')) { ns.httpworm(h.hostname);  }
+            if (ns.fileExists('relaySMTP.exe', 'home')) { ns.relaysmtp(h.hostname); }
+            if (ns.fileExists('SQLInject.exe', 'home')) { ns.sqlinject(h.hostname); }
+         }
+
+         if (h.openPortCount >= h.numOpenPortsRequired) {
+            
+            ns.nuke(h.hostname);
+            ns.tprintf(`${c.magenta}Did nuke() ${h.hostname}. Don't forget the backdoor !!!${c.reset}`);
+         } else{
+            ns.tprintf(`${c.red}Cannot nuke() ${h.hostname}.${c.reset}`);
+         }
+      },
+
+      /**
        * Method: Scan the actual subnet for hosts and remember root.
        */
       scan: (hostname) => {
@@ -52,7 +77,7 @@ export async function main(ns) {
          if (! target) mns.exit('ERROR ARGS :: String expected.')
 
          // Greetings new run ...
-         ns.tprintf(`${c.yellow}restart run on ${d.getdate()} at ${d.gettime()}${c.reset}`);
+         ns.tprintf(`${c.yellow}reboot on ${d.getdate()} at ${d.gettime()}${c.reset}`);
 
          //we start with the hosts on our own subnet.
          const hackit    = '/scripts/hackit.js';
@@ -65,42 +90,18 @@ export async function main(ns) {
             
             let h = ns.getServer(host);
 
+            if (! h.hasAdminRights) mns.exploit(h);
             if (! h.purchasedByPlayer) hosts = hosts.concat(mns.scan(h.hostname));
             if (  h.purchasedByPlayer || h.hasAdminRights) {
 
-               // At first we have to check, if hackit is already running!
-               let ps = ns.ps(h.hostname);
-
-                     // DEBUG
-                     //ns.tprintf(`${c.magenta}DEBUG :: typeof ps ${typeof ps}${c.reset}`);
-
-               for (let p of ps) {
-
-                     // DEBUG
-                     //ns.tprintf(`${c.magenta}DEBUG :: ${p.filename}, pid(${p.pid}) on ${h.hostname} ...${c.reset}`);
-
-                  if ('/'+p.filename == hackit) {
-
-                     ns.tprintf(`${c.yellow}WARNING :: hackit with pid(${p.pid}) is running on ${h.hostname}. Trying to kill it.${c.reset}`);
-
-                     if (ns.kill(p.pid)) { ps = null }
-                     else {
-                        ns.tprintf(`${c.red}ERROR :: hackit on ${h.hostname} and cannot be killed!${c.reset}`);
-                     }
-                  }
-               }
-            
-               // If the target's RAM 0GB, we cannot run scripts on it.
                const max = ns.getServerMaxRam(h.hostname); 
 
-               if (max > 0 && !ps) {
+               if (max > 0) {
 
                   const threads = Math.floor(max / scriptram); 
 
-                  if (! ns.fileExists(hackit, h.hostname))
-                     ns.scp( hackit, h.hostname);
-                     ns.exec(hackit, h.hostname, threads, target);
-
+                  ns.scp(hackit, h.hostname);
+                  ns.exec(hackit, h.hostname, threads, target);
                   ns.tprintf(`${c.green}SUCCESS :: hackit is running on ${h.hostname} and hacking ${target}${c.reset}`);
                }
             }
