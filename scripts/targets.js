92c474f55e9614949755b016670495190db410f9
m0rphisto <warpfactor.net@gmail.com>
2023-08-02 10:18:17 +0200

    Complete rework, shrinked to 41 lines.

--- scripts/targets.js
+++ scripts/targets.js
@@ -1,5 +1,5 @@
 /** 
- * $Id: targets.js v0.6 2023-08-02 01:25:30 CEST 3.85GB .m0rph $
+ * $Id: targets.js v0.6 2023-08-02 10:14:55 CEST 3.80GB .m0rph $
  * 
  * descripttion:
  *    Get the r00ted and backdoored servers and log their stats.
@@ -14,64 +14,28 @@ export async function main(ns) {
 
    'use strict';
 
-   const mns = {
+   let i = 0, data = '', scanned = new Set(['home']);
+   const logfile = `/log/targets-${d.timestamp()}.js`;
 
-      /**
-       * Property: Timestamp for multiple file access on the same logfile.
-       */
-      timestamp: d.timestamp(),
+   ns.tprintf(`${c.cyan}Start targets run at: ${d.getdate()}, ${d.gettime()}${c.reset}`);
+   ns.tprintf(`${c.cyan}Note: ONLY r00ted servers !!!${c.reset}`);
 
-      /**
-       * Property: Hosts, that were already root for subnet scanning. 
-       */
-      scanned: new Set(['home']),
+   scanned.forEach(a => ns.scan(a).forEach(b => scanned.add(b)));
+   scanned.forEach(host => {
 
+      let line, h = ns.getServer(host);
 
-      /**
-       * Method: Initialization of the mns object.
-       */
-      init () {
+      if (! h.purchasedByPlayer && h.hasAdminRights) {
 
-         // Initialize logfile ...
-         this.logfile = `/log/targets-${this.timestamp}.js`;
-
-         // ... and scan the network.
-         this.scanned.forEach(a => ns.scan(a).forEach(b => this.scanned.add(b)));
-      },
-
-      log (data, mode) {
-         // mode: w = overwrite complete file, a = append data
-         let m = (mode) ? mode : 'w';
-         ns.tprintf(`${c.cyan}Writing data to logfile ${this.logfile}.${c.reset}`);
-         ns.write(this.logfile, data, m);
-      },
-
-      print () {
-
-         ns.tprintf(`${c.cyan}Start targets run at: ${d.getdate()}, ${d.gettime()}${c.reset}`);
-         ns.tprintf(`${c.cyan}Note: ONLY r00ted servers !!!${c.reset}`);
-
-         let i = 0, data = '', host;
-
-         this.scanned.forEach(host => {
-
-            let color, line, h = ns.getServer(host);
-
-            if (! h.purchasedByPlayer && h.hasAdminRights) {
-
-               line = sprintf(
-                  `${h.hostname}: ram(${ns.formatRam(h.maxRam)}), max(\$${ns.formatNumber(h.moneyMax)}), hack(${h.requiredHackingSkill}), growth(${h.serverGrowth}), sec(${h.minDifficulty})`
-               );
+         line = sprintf(
+            `${h.hostname}: ram(${ns.formatRam(h.maxRam)}), max(\$${ns.formatNumber(h.moneyMax)}), hack(${h.requiredHackingSkill}), growth(${h.serverGrowth}), sec(${h.minDifficulty})`
+         );
                   
-               ns.tprintf(`${h.maxRam == 0 ? c.red : c.cyan}${++i}) ${line}${c.reset}`);
-               data = `${data}${line}\n`;
-            }
-         });
-         this.log(data);
+         ns.tprintf(`${h.maxRam == 0 ? c.red : c.cyan}${++i}) ${line}${c.reset}`);
+         data = `${data}${line}\n`;
       }
-   };
+   });
 
-   mns.init();
-   mns.print();
+   ns.tprintf(`${c.cyan}Writing data to logfile ${logfile}.${c.reset}`);
+   ns.write(logfile, data, 'w');
 }
-
