a92d0576d5904fbc0fcc92ebd719e29b45297d40
m0rphisto <warpfactor.net@gmail.com>
2023-08-02 09:50:46 +0200

    Complete rework, shrinked to 40 lines.

--- scripts/getnet.js
+++ scripts/getnet.js
@@ -1,90 +1,40 @@
 /** 
- * $Id: getnet.js v0.5 2023-08-01 23:48:03 CEST 5.25GB .m0rph $
+ * $Id: getnet.js v0.6 2023-08-02 09:40:50 CEST 3.80GB .m0rph $
  * 
  * descripttion:
  *    First step: Get the complete network and write them to getnet.log.js.
  *   Second step: Create a JSON file. (TODO)
  * 
  * @param {NS} ns
- * @param      ns.args[0]  Hostname where the subnet is to be scanned.
  */
 
 import {c} from '/modules/colors.js';
 import {d} from '/modules/datetime.js';
-import {a} from '/modules/arguments.js';
 
 export async function main(ns) {
 
    'use strict';
 
-   const mns = {
+   let  i = 0, data = '', scanned = new Set(['home']);
+   const logfile = `/log/getnet-${d.timestamp()}.js`;
 
-      /**
-       * Property: Timestamp for multiple file access on the same logfile.
-       */
-      timestamp: d.timestamp(),
+   ns.tprintf(`${c.cyan}Start getnet run at: ${d.getdate()}, ${d.gettime()}${c.reset}`);
 
-      /**
-       * Property: Hosts, that were already root for subnet scanning. 
-       */
-      scanned: new Set(['home']),
+   scanned.forEach(a => ns.scan(a).forEach(b => scanned.add(b)));
+   scanned.forEach(host => {
 
+      let line, h = ns.getServer(host);
 
-      /**
-       * Method: Initialization of the mns object.
-       */
-      init () {
-         this.logfile = `/log/getnet-${this.timestamp}.js`;
-      },
+      if (! h.purchasedByPlayer) {
 
-      /**
-       * Method: Validate passed argument.
-       */
-      check (arg) {
-         return a.str(arg) ? ns.serverExists(arg) ? arg : undefined : undefined;
-      },
-
-      /**
-       * Method: Scan the actual subnet for hosts and remember root.
-       */
-      scan (hostname) {
-         let host = (hostname) ? hostname : ns.getHostname();
-         this.scanned.add(host);
-         return ns.scan(host);
-      },
-
-      log (data, mode) {
-         let m = (mode) ? mode : 'w';
-         ns.tprintf(`${c.cyan}Writing data to logfile${c.reset}`);
-         ns.write(this.logfile, data, m);
-      },
-
-      print (hosts) {
-
-         ns.tprintf(`${c.cyan}Start getnet run at: ${d.getdate()}, ${d.gettime()}${c.reset}`);
-
-         let i = 0, data = '', host, req = '';
-
-         while (host = hosts.shift()) {
-
-            if (this.scanned.has(host))  continue;
-            
-            let line, h = ns.getServer(host);
-
-            if (! h.purchasedByPlayer) {
-               hosts = hosts.concat(this.scan(h.hostname));
-               line = sprintf(
-                  `${h.hostname}: ip(${h.ip}), hack(${h.requiredHackingSkill}), ports(${h.numOpenPortsRequired}), root(${(h.hasAdminRights)}), backdoor(${(h.backdoorInstalled)})`
-               );
-               ns.tprintf(`${c.cyan}${++i}) ${line}${c.reset}`);
-               data = `${data}${line}\n`;
-            }
-         }
-         this.log(data);
+         line = sprintf(
+            `${h.hostname}: ip(${h.ip}), hack(${h.requiredHackingSkill}), ports(${h.numOpenPortsRequired}), root(${(h.hasAdminRights)}), backdoor(${(h.backdoorInstalled)})`
+         );
+         ns.tprintf(`${c.cyan}${++i}) ${line}${c.reset}`);
+         data = `${data}${line}\n`;
       }
-   };
-
-   mns.init();
-   mns.print(mns.scan(a.count(ns.args, 1) ? mns.check(ns.args[0]) : ns.getHostname()));
+   });
+      
+   ns.tprintf(`${c.cyan}Writing data to logfile${c.reset}`);
+   ns.write(logfile, data, 'w');
 }
-
