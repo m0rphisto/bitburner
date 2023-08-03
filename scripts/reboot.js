42beab2af9d736ed0b9406efa48ef47cb3e960a3
m0rphisto <warpfactor.net@gmail.com>
2023-08-03 18:28:29 +0200

    Complete rework...# Please enter the commit message for your changes. Lines starting

--- scripts/reboot.js
+++ scripts/reboot.js
@@ -1,9 +1,5 @@
 /** 
- * filename: reboot.js
- *     date: 2023-07-29
- *  version: 0.6
- *   author: .m0rph
- *      RAM: 7.15GB
+ * $Id: reboot.js v0.8 2023-08-03 03:23:32 CEST 6.05GB .m0rph $
  * 
  * description:
  *    In case of an infinite loop, respectively game crash or augment installation (soft reset)
@@ -15,7 +11,10 @@
 
 import {c} from '/modules/colors.js';
 import {d} from '/modules/datetime.js';
-import {a} from '/modules/arguments.js';
+import {
+   has_count,
+   is_string
+} from '/modules/arguments.js';
 
 export function autocomplete(data, args) {
    return [...data.servers];
@@ -25,123 +24,101 @@ export async function main(ns) {
 
    'use strict';
 
-   const mns = {
+   /**
+    * Error exit handler.
+    * 
+    * @param {string}   msg   Error message.
+    */
+   function exit (msg) {
+      ns.tprintf(`${c.red}ERROR: ${msg} Exiting !!!${c.reset}`);
+      ns.exit();
+   }
 
-      /**
-       * Property: Hosts, that were already root for subnet scanning. 
-       */
-      scanned: ['home'],
+
+   /**
+    * At first the parameter validation.
+    */
+   has_count(ns.args, 0) ? exit('No target passed') : null;
+
+   has_count(ns.args, 1)
+      ? is_string(ns.args[0]) 
+         ? ns.serverExists(ns.args[0]) ? null : exit(`Target ${ns.args[0]} does not exist.`)
+         : exit(`ERROR :: ${ns.args[0]} :: String expected.`)
+      : null;
 
 
-      /**
-       * Method: Validate passed argument.
-       */
-      check (arg) {
-         return a.str(arg) ? ns.serverExists(arg) ? arg : this.exit('Target does not exist.') : undefined;
-      },
+
+   // Greetings new run ...
+   ns.tprintf(`${c.yellow}reboot on ${d.getdate()} at ${d.gettime()}${c.reset}`);
+
+   let scanned  = new Set(['home']);
+   const target = ns.args[0], hackit = '/scripts/hackit.js';
+
+   scanned.forEach(a => ns.scan(a).forEach(b => scanned.add(b)));
+   scanned.forEach(host => {
+
+      const h = ns.getServer(host);
 
       /**
        * Method: No backdoor? No admin rights? OK, let's get'em.
        */
-      exploit (h) {
+      if (h.numOpenPortsRequired > 0 && !h.hasAdminRights) {
 
-         if (h.numOpenPortsRequired > 0) {
+         const port_opener = ['BruteSSH', 'FTPCrack', 'HTTPWorm', 'relaySMTP', 'SQLInject'];
+         const open_port   = (port) => {
 
-            const port_opener = ['BruteSSH', 'FTPCrack', 'HTTPWorm', 'relaySMTP', 'SQLInject'];
-            const open_port   = (port) => {
-
-               if (ns.fileExists(`${port}.exe`)) {
-                  let opener = `ns.${port.toLowerCase()}(h.hostname)`;
-                  try {
-                     eval(opener);
-                  } catch (e) {
-                     ns.tprintf(`${c.red}ERROR: ${e}${reset}`);
-                  }
-               }
-            };
-
-            port_opener.forEach(open_port);
-         }
-
-         if (h.openPortCount >= h.numOpenPortsRequired) {
-            
-            /**
-             * We have to await the h.hasAdminRights condition !!!
-             * Otherwise reboot will walk through all hosts and do just one job,
-             * exploit it, nuke it or start hackit on it.
-             */
-
-            ns.nuke(h.hostname);
-            ns.tprintf(`${c.magenta}Did nuke() ${h.hostname}. Don't forget the backdoor !!!${c.reset}`);
-            ns.sleep(1000); // (is 500ms enough?)                                  |
-         } else{
-            ns.tprintf(`${c.red}Cannot nuke() ${h.hostname}.${c.reset}`);
-         }
-      },
-
-      /**
-       * Method: Scan the actual subnet for hosts and remember root.
-       */
-      scan (hostname) {
-         let host = (hostname) ? hostname : ns.getHostname();
-         if (! this.scanned.includes(host)) this.scanned.push(host);
-         return ns.scan(host);
-      },
-
-      /**
-       * Method: Get all servers and restart hackit on them.
-       */
-      run (target) {
-
-         if (! target) this.exit('ERROR ARGS :: String expected.')
-
-         // Greetings new run ...
-         ns.tprintf(`${c.yellow}reboot on ${d.getdate()} at ${d.gettime()}${c.reset}`);
-
-         //we start with the hosts on our own subnet.
-         const hackit    = '/scripts/hackit.js';
-         let host, hosts = ns.scan();
-
-         while (host = hosts.shift()) {
-
-            if (this.scanned.includes(host)) continue;
-            
-            let h = ns.getServer(host);
-
-            if (! h.hasAdminRights) this.exploit(h);
-            if (! h.purchasedByPlayer) hosts = hosts.concat(this.scan(h.hostname));
-            if (  h.purchasedByPlayer || h.hasAdminRights) {
-
-               const max = ns.getServerMaxRam(h.hostname); 
-
-               if (max > 0) {
-
-                  const threads = Math.floor(max / ns.getScriptRam(hackit));
-
-                  if (! ns.fileExists(hackit, h.hostname)) {
-                  
-                     ns.scp(hackit, h.hostname);
-
-                     ns.exec(hackit, h.hostname, threads, target);
-                     ns.tprintf(`${c.green}OK, hackit is running on ${h.hostname} and hacking ${target}.${c.reset}`);
-                  } else {
-                     ns.tprintf(`${c.magenta}OK, hackit already is running on ${h.hostname} and hacking ???${c.reset}`);
-                  }
-               } else {
-                  ns.tprintf(`${c.yellow}WARNING: ${h.hostname} doesn't have any RAM. You should do a targets() run !!!${c.reset}`);
+            if (ns.fileExists(`${port}.exe`)) {
+               let opener = `ns.${port.toLowerCase()}(h.hostname)`;
+               try {
+                  eval(opener);
+               } catch (e) {
+                  ns.tprintf(`${c.red}ERROR: ${e}${reset}`);
                }
             }
-         }
-      },
+         };
 
-      /**
-       * Method: Error handler.
-       */
-      exit (msg) {
-         ns.tprintf(`${c.red}${msg} Exiting !!!${c.reset}`);
-         ns.exit();
+         port_opener.forEach(open_port);
       }
-   };
 
-   mns.run(a.count(ns.args, 1) ? mns.check(ns.args[0]) : mns.exit('No target passed'));
+      if (h.openPortCount >= h.numOpenPortsRequired && !h.hasAdminRights) {
+         
+         /**
+          * We have to await the h.hasAdminRights condition !!!
+          * Otherwise reboot will walk through all hosts and do just one job,
+          * exploit it, nuke it or start hackit on it.
+          */
+         ns.nuke(h.hostname);
+
+         ns.tprintf(`${c.magenta}Did nuke() ${h.hostname}. Don't forget the backdoor !!!${c.reset}`);
+      }
+      //else {
+      if (h.hasAdminRights) {
+         ns.tprintf(`${c.green}${h.hostname} already r00ted.${c.reset}`);
+      }
+
+
+      if (h.hasAdminRights) {
+
+         if (h.maxRam > 0) {
+
+            const threads = Math.floor((h.maxRam - h.ramUsed) / ns.getScriptRam(hackit));
+
+            
+            if (! ns.fileExists(hackit, h.hostname)) {
+            
+               ns.tprintf(`${c.cyan}Copying hackit to ${h.hostname} ... ${ns.scp(hackit, h.hostname) ? 'OK' : 'FAILED'}.${c.reset}`);
+            } 
+            
+            if (threads > 0) {
+
+               ns.exec(hackit, h.hostname, threads, target)
+                  ? ns.tprintf(`${c.green}OK, hackit is running on ${h.hostname} and hacking ${target}.${c.reset}`)
+                  : ns.tprintf(`${c.red}ERROR: Could not start hackit ${h.hostname}.${c.reset}`)
+            }
+         }
+         else {
+            ns.tprintf(`${c.yellow}WARNING: ${h.hostname} doesn't have any RAM. You should do a targets() run !!!${c.reset}`);
+         }
+      }
+   });
 }
