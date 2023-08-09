2496f5f26d284abd5135bb9edca088e17f754ef3
m0rphisto <warpfactor.net@gmail.com>
2023-08-09 22:27:02 +0200

    Complete rework due to skill improvements.

--- scripts/purchase.js
+++ scripts/purchase.js
@@ -1,47 +1,46 @@
 /** 
- * filename: purchase.js
- *     date: 2023-06-29
- *  version: 0.2
- *   author: .m0rph
- *      RAM: 8.60GB
+ * Id: purchase.js v0.3 2023-08-09 20:54:45 6.60GB .m0prh $
+ * 
+ *  pserv-maxRam: 2 ** 20 == 1049576 
  * 
  * @param {NS} ns
  *             ns.args[0]  number   RAM size
- *             ns.args[1]  number   Number of purchase run (prefix)
- *             ns.args[2]  string   Target server (MUST be a valid hostname)
+ *             ns.args[1]  string   Target server (MUST be a valid hostname)
  */
 export async function main(ns) {
 
    if (ns.args.length > 0) {
       if (
          typeof ns.args[0] === 'number' && 
-         typeof ns.args[1] === 'number' && 
-         typeof ns.args[2] === 'string' &&
-         ns.serverExists(ns.args[2])
+         typeof ns.args[1] === 'string' &&
+         ns.serverExists(ns.args[1])
       ) {
-         const hackit    = '/scripts/hackit.js';
-         const ram       = ns.args[0];
-         const prefix    = ns.args[1];
-         const target    = ns.args[2];
-         const scriptram = ns.getScriptRam(hackit);
-         const threads   = Math.floor(ram / scriptram); // Just cut the fraction ... (64.803 -> 64)
-         const cost      = ns.getPurchasedServerCost(ram);
-         const limit     = ns.getPurchasedServerLimit();
-         const buyed     = ns.getPurchasedServers();
+         const
+            hackit    = '/scripts/hackit.js',
+            ram       = ns.args[0],
+            target    = ns.args[1],
+            scriptram = ns.getScriptRam(hackit),
+            threads   = Math.floor(ram / scriptram), // Just cut the fraction ... (64.803 -> 64)
+            cost      = ns.getPurchasedServerCost(ram),
+            limit     = ns.getPurchasedServerLimit(),
+            buyed     = ns.getPurchasedServers();
          
          let i = 0;
-         while (i < limit && buyed != limit) {
+         while (i < limit && buyed != limit)
+         {
             let money = ns.getServerMoneyAvailable('home');
             // Continously try to purchase a server until we've reached the
             // maximum amount of servers.
-            if (money > cost){
-               let hostname = ns.purchaseServer('pserv-' + prefix + '-' + i, ram);
+            if (money > cost)
+            {
+               let hostname = ns.purchaseServer(`pserv-${i++}`, ram);
                ns.scp(hackit, hostname);
                ns.exec(hackit, hostname, threads, target);
-               ns.printf("SUCCESS :: Purchased %s is running and hacking %s.", hostname, target)
-               ++i;
-            } else {
-               ns.printf('SeverCost is at %s. Not enough money!', ns.formatNumber(cost));
+               ns.printf(`SUCCESS :: Purchased ${hostname} is running and hacking ${target}.`)
+            }
+            else
+            {
+               ns.printf(`SeverCost is at ${ns.formatNumber(cost)}. Not enough money!`); 
             }
 
             // We need to wait for a second, otherwise the script will
@@ -53,8 +52,7 @@ export async function main(ns) {
          ns.tprint(
             "ERROR ARGS :: "+
                "\n\targs[0]->(NUMBER || valid RAM size),"+
-               "\n\targs[1]->(NUMBER || purchase run number)"+
-               "\n\targs[2]->(STRING || valid hostname)."+
+               "\n\targs[1]->(STRING || valid hostname)."+
                "\n\nExiting !!!"
          );
       }
--- scripts/purchdel.js
+++ scripts/purchdel.js
@@ -1,11 +1,7 @@
 /** 
- * filename: purchdel.js
- *     date: 2023-07-21
- *  version: 0.1
- *   author: .m0rph
- *      RAM: 6.30GB
+ * $Id: purchdel.js v0.2 2023-08-09 21:56:14 6.80GB .m0rph $
  * 
- * @param {NS} ns
+ * @param {NS} ns The Netscript API.
  */
 
 import {c} from '/modules/colors.js';
@@ -14,37 +10,17 @@ export async function main(ns) {
 
    'use strict';
 
-   const hackit   = 'scripts/hackit.js';
    const pservers = ns.getPurchasedServers();
    let   count    = 0;
 
    ns.tprintf(`${c.cyan}We have ${pservers.length} purchased servers available.${c.reset}`);
 
-   for (let pserver of pservers){
-
-      // At first we have to kill running scripts!
-      let ps = ns.ps(pserver);
-
-      for (let p of ps) {
-
-         if (p.filename == hackit) {
-            ns.tprintf(`${c.cyan}> ${c.yellow}Killing hackit with pid(${p.pid}) on ${pserver} ...${c.reset}`);
-         }        
-
-         if (ns.kill(p.pid)) { ps = null }
-         else {
-            ns.tprintf(`${c.cyan}> ${c.red}ERROR :: hackit on ${c.yellow}${pserver}${c.reset} and cannot be killed!${c.reset}`);
-         }
-      }        
-
-      if (!ps) {
-         ns.tprintf(`${c.cyan}> Deleting ${pserver} ...${c.reset}`);
-         ns.deleteServer(pserver);
-         count++;
-      }
-      else {
-         ns.tprintf(`${c.cyan}> Purchased server ${pserver} cannot be deleted!${c.reset}`);
-      }
+   for (let pserver of pservers)
+   {
+      ns.killall(pserver);
+      ns.deleteServer(pserver);
+      count++;
    }
+   
    ns.tprintf(`${c.cyan}${count} Servers deleted. Ready to purchase bigger ones!`);
 }
