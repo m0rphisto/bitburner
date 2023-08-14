0cc8fc77fc9469afe249ab0771fa48bac7313742
m0rphisto <warpfactor.net@gmail.com>
2023-08-14 22:37:46 +0200

    Bugfix: home's subnet hosts -> connect;etc

--- scripts/firstsub.js
+++ scripts/firstsub.js
@@ -1,5 +1,5 @@
 /** 
- * $Id: firstsub.js v0.2 2023-08-14 21:27:30 CEST 4.75GB .m0rph $
+ * $Id: firstsub.js v0.2 2023-08-14 21:27:30 CEST 4.80GB .m0rph $
  * 
  * description:
  *    After augmentation installation we have to start at the beginning,
@@ -48,7 +48,7 @@ export async function main(ns) {
    for (let i = 0; i < hosts[ports].length; i++)
    {
       let host = hosts[ports][i];
-
+      
       if (ns.fileExists('BruteSSH.exe'))  ns.brutessh(host);
       if (ns.fileExists('FTPCrack.exe'))  ns.ftpcrack(host);
       if (ns.fileExists('HTTPWorm.exe'))  ns.httpworm(host);
@@ -56,33 +56,58 @@ export async function main(ns) {
       if (ns.fileExists('SQLInject.exe')) ns.sqlinject(host);
 
       ns.nuke(host);
-      ns.tprintf(`${c.magenta}Did nuke() ${host}.${c.reset}`);
-   
-      if (ns.getServerMaxRam(host) > 0)
+      if (ns.hasRootAccess(host))
       {
-         ns.run(deploy, 1, host);
-         ns.run(looper, 1, host);
+         // Any further actions only make sense, if we 0wned the box.
+         ns.tprintf(`${c.magenta}Did nuke() ${host}.${c.reset}`);
+   
+         if (ns.getServerMaxRam(host) > 0)
+         {
+            ns.run(deploy, 1, host);
+            ns.run(looper, 1, host);
 
-         ns.tprintf(`${c.magenta}Did deploy ${host} and start the looper master.${c.reset}`);
+            ns.tprintf(`${c.magenta}Did deploy ${host} and started the looper master.${c.reset}`);
+         }
+         else
+         {
+            ns.run(weaken, threads, host); await ns.sleep(1000);
+            ns.run(grow,   threads, host); await ns.sleep(1000);
+            ns.run(hack,   threads, host); await ns.sleep(1000);
+
+            ns.tprintf(`${c.magenta}Did locally start weaken(), grow() and mhack() for ${host}.${c.reset}`);
+         }
+
+         switch (ports)
+         {
+            // Only hosts in home's subnet are directly connectable.
+
+            case 0:
+               host = `${host == 'nectar-net'    ? 'sigma-cosmetics; connect nectar-net'   : host}`;
+               break;
+            case 1:
+               host = `${host == 'max-hardware'  ? 'sigma-cosmetics; connect max-hardware' : host}`;
+               host = `${host == 'zer0'          ? 'hong-fang-tea; connect zer0'           : host}`;
+               host = `${host == 'CSEC'          ? 'iron-gym; connect CSEC'                : host}`;
+               host = `${host == 'neo-net'       ? 'CSEC; connect neo-net'                 : host}`;
+               break;
+            case 2:
+               host = `${host == 'silver-helix'  ? 'max-hardware; connect silver-helix'    : host}`;
+               host = `${host == 'phantasy'      ? 'phantasy; connect neo-net'             : host}`;
+               host = `${host == 'omega-net'     ? 'nectar-net; connect omega-net'         : host}`;
+               host = `${host == 'avmnite-02h'   ? 'omega-net; connect avmnite-02h'        : host}`;
+               host = `${host == 'johnson-ortho' ? 'neo-net; connect johnson-ortho'        : host}`;
+               host = `${host == 'the-hub'       ? 'silver-helix; connect the-hub'         : host}`;
+               host = `${host == 'crush-fitness' ? 'neo-net; connect crush-fitness'        : host}`;
+               break;
+         }
+
+         shell(`home; connect ${host}; backdoor`);
+         await ns.sleep(6000);
+
+         ns.tprintf(`${c.magenta}And finaly installed the backdoor on ${host}.${c.reset}`);
       }
       else
-      {
-         ns.run(weaken, threads, host); await ns.sleep(1000);
-         ns.run(grow,   threads, host); await ns.sleep(1000);
-         ns.run(hack,   threads, host); await ns.sleep(1000);
-
-         ns.tprintf(`${c.magenta}Did start weaken(), grow() and mhack() for ${host}.${c.reset}`);
-      }
-
-      if (ports == 0)
-      {
-         // Only null port hosts are directly connectable, except nectar-net.
-         shell(`connect ${host}; backdoor`);
-         await ns.sleep(5000);
-         shell(`home`);
-      }
-
-      ns.tprintf(`${c.magenta}And finaly installed the backdoor on ${host}.${c.reset}`);
+         ns.tprintf(`${c.magenta}Not able to nuke() ${host}. Check the darkweb !!!${c.reset}`);
    }
 }
 
