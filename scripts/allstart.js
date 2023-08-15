3f6ccf3e905768c40d1c02ce6d69457d17bb6df3
m0rphisto <warpfactor.net@gmail.com>
2023-08-15 19:38:49 +0200

    Complete rework pserver <--> nullRAMer.

--- scripts/allstart.js
+++ scripts/allstart.js
@@ -1,5 +1,5 @@
 /**
- * $Id: allstart.js v0.6 2023-08-14 20:31:58 5.25GB .m0rph $
+ * $Id: allstart.js v0.7 2023-08-14 23:11:47 5.90GB .m0rph $
  * 
  * description:
  *    Restarts all looper scripts on hacked and on purchased servers.
@@ -7,104 +7,104 @@
  * Note:
  *    This script needs two options, or it does NOTHING.
  * 
- *    -l Start looper scripts.
- *    -p Start purchased server scripts.
+ *    -s Start server looper scripts.
+ *    -p Start purchased server looper scripts.
  * 
- *    And don't forget to adjust the threads settings due to the
- *    local RAM amount!
+ *    --threads   How many threads for the local looper scripts?
+ *                Default is set to 512.
  * 
  * 
  * @param {NS} ns The Netscript API.
  */
 
 import {c} from '/modules/colors.js';
-import {has_option} from '/modules/arguments.js';
+import {has_option, get_option} from '/modules/arguments.js';
 
 export async function main(ns) {
 
-// System crashed? OK, let it run again.
+   // System crashed? OK, let it run again.
 
-const
-   threads = 512,
-   target  = 'megacorp',
-   ram =
-      [
-         // Self-targeting hosts ...
-         'n00dles', 'foodnstuff', 'sigma-cosmetics', 'joesguns', 'hong-fang-tea', 'harakiri-sushi', 'iron-gym', 'max-hardware',
-         'CSEC', 'nectar-net', 'zer0', 'silver-helix', 'phantasy', 'neo-net', 'omega-net', 'netlink', 'avmnite-02h',
-         'the-hub', 'summit-uni', 'zb-institute', 'I.I.I.I', 'rothman-uni', 'catalyst', 'millenium-fitness',
-         'rho-construction', 'lexo-corp', 'alpha-ent', 'aevum-police', 'global-pharm', 'omnia', 'unitalife', 'univ-energy',
-         'solaris', 'microdyne', 'titan-labs', 'run4theh111z', 'fulcrumtech', 'helios', 'vitalife', 'omnitek', '.',
-         'powerhouse-fitness', 'blade'
-      ],
-   no_ram =
-      [
-         // Externally attacked hosts ...
-         'johnson-ortho', 'computek', 'crush-fitness', 'syscore', 'galactic-cyber', 'aerocorp', 'snap-fitness', 'deltaone',
-         'defcomm', 'zeus-med', 'icarus', 'infocomm', 'nova-med', 'taiyang-digital', 'zb-def', 'applied-energetics',
-         'stormtech', '4sigma', 'kuai-gong', 'b-and-a', 'nwo', 'clarkinc', 'The-Cave', 'ecorp', 'fulcrumassets',
-         'megacorp'
-      ];
+   const 
+      threads = get_option(ns, '--threads') ?? 512, 
+      deploy  = '/looper/deploy.js',
+      master  = '/looper/master.js',
+      weaken  = '/looper/weaken.js',
+      grow    = '/looper/grow.js',
+      mhack   = '/looper/mhack.js',
+      hack    = '/looper/hack.js';
 
+   let hosts  = new Set(['home']); // Get a complete r00ted hosts list.
+   hosts.forEach(a => ns.scan(a).forEach(b => b.match('pserv') ?? hosts.add(b).delete('home')));
 
    
-   if (has_option(ns, '-l'))
+   if (has_option(ns, '-s'))
    {
-      ns.killall(); // At first we kill all local running scripts ...
+      ns.killall(); // At first we kill all local running scripts, ...
 
-      ram.forEach(host => {
-         if (
-            // ... and then we check the self-targeting hosts.
-            ns.isRunning('/looper/weaken.js', host, '') ||
-            ns.isRunning('/looper/grow.js',   host, '') ||
-            ns.isRunning('/looper/hack.js',   host, '')
-         ) {
-            ns.tprintf(`${c.white}One of the H/G/W scripts active on ${host}. Trying to kill it ... ${ns.killall(host) ? 'OK' : 'FAILED'}.${c.reset}`);
-         } 
-
-         // Only just in case the hosts wasn't already deployed.
-         if (!ns.hasRootAccess(host)) ns.run('/looper/deploy.js', 1, host);
-         if ( ns.hasRootAccess(host))
-         {
-            // Finally get them running again.
-            ns.tprintf(`${c.cyan}Starting local /looper/master.js for ${host}.${c.reset}`);
-            ns.run('/looper/master.js', 1, host);
-         }
-         else
-            ns.tprintf(`${c.magenta}No root access to ${host}, so not starting looper master !!!${c.reset}`);
-      });
-
-      for (let nram of no_ram)
+      for (let host of hosts)
       {
-         // Only just in case the hosts wasn't already r00ted.
-         if (!ns.hasRootAccess(nram)) ns.run('/looper/deploy.js', 1, nram);
-         if ( ns.hasRootAccess(nram))
+         if (ns.getServerMaxRam(host) > 0)
          {
-            // Finally get them locally running again.
-            ns.tprintf(`${c.cyan}Starting local /looper/weaken.js ${nram} -t ${threads}${c.reset}`);
-            ns.run('/looper/weaken.js', threads, nram);
-            await ns.sleep(1000);
+            // ... and then we check the self-targeting hosts.
 
-            ns.tprintf(`${c.cyan}Starting local /looper/grow.js ${nram} -t ${threads}${c.reset}`);
-            ns.run('/looper/grow.js', threads, nram);
-            await ns.sleep(1000);
-      
-            ns.tprintf(`${c.cyan}Starting local /looper/mhack.js ${nram} -t ${threads}${c.reset}`);
-            ns.run('/looper/mhack.js', threads, nram);
-            await ns.sleep(1000);
+            if (
+               ns.isRunning(weaken, host, '') ||
+               ns.isRunning(grow,   host, '') ||
+               ns.isRunning(hack,   host, '')
+            ) {
+               ns.tprintf(`${c.white}One of the H/G/W scripts active on ${host}. Trying to kill it ... ${ns.killall(host) ? 'OK' : 'FAILED'}.${c.reset}`);
+            } 
+
+            // Only just in case the hosts wasn't already deployed.
+            if (!ns.hasRootAccess(host)) ns.run(deploy, 1, host);
+            if ( ns.hasRootAccess(host))
+            {
+               // Finally get them running again.
+               ns.tprintf(`${c.cyan}Starting local ${master} for ${host}.${c.reset}`);
+               ns.run(master, 1, host);
+            }
+            else
+               ns.tprintf(`${c.magenta}No root access to ${host}, so not starting looper master !!!${c.reset}`);
          }
          else
-            ns.tprintf(`${c.magenta}No root access to ${nram}, so not starting looper master !!!${c.reset}`)
+         {
+            // Only just in case the hosts wasn't already r00ted.
+            if (!ns.hasRootAccess(host)) ns.run(deploy, 1, host);
+            if ( ns.hasRootAccess(host))
+            {
+               // Finally get them locally running again.
+               ns.tprintf(`${c.cyan}Starting local ${weaken} ${host} -t ${threads}${c.reset}`);
+               ns.run(weaken, threads, host);
+               await ns.sleep(1000);
+   
+               ns.tprintf(`${c.cyan}Starting local ${grow} ${host} -t ${threads}${c.reset}`);
+               ns.run(grow, threads, host);
+               await ns.sleep(1000);
+         
+               ns.tprintf(`${c.cyan}Starting local ${mhack} ${host} -t ${threads}${c.reset}`);
+               ns.run(mhack, threads, host);
+               await ns.sleep(1000);
+            }
+            else
+               ns.tprintf(`${c.magenta}No root access to ${host}, so not starting looper master !!!${c.reset}`);
+         }
       }
    }
 
+
    if (has_option(ns, '-p'))
    {
-      for (let i = 0; i < 25; i++)
-      {
+      /**
+       * OK, we have 25 servers with 0.00GB RAM. We also have 25 purchased servers.
+       * So we can let exactly one purchased server attack one null RAMmer. ;-)
+       */
+      let i = 0;
+      hosts.forEach(a => ns.getServerMaxRam(a) > 0 && hosts.delete(a));
+      hosts.delete('The-Cave'); // Has 0GB but $0 max also
+      hosts.delete('darkweb');  // Has 0GB but $0 max also
+      hosts.forEach(nram => {
          [
-            '/modules/colors.js', '/modules/arguments.js', '/modules/datetime.js',
-            '/looper/weaken.js', '/looper/grow.js', '/looper/hack.js', '/looper/master.js'
+            '/modules/colors.js', '/modules/arguments.js', '/modules/datetime.js', weaken, grow, hack, master
          ].forEach(file => {
             if (!ns.fileExists(file, `pserv-${i}`))
             {
@@ -114,20 +114,22 @@ const
             }
          });
 
-         if (ns.hasRootAccess(target))
+         if (ns.hasRootAccess(nram))
          {
             // Any running scripts?
             ns.killall(`pserv-${i}`);
 
             // OK, starting the looper master.
-            ns.tprintf(`${c.cyan}pserv-${i}: Starting /looper/master.js ${target} true${c.reset}`);
-            let pid = ns.exec('/looper/master.js', `pserv-${i}`, 1, ...[target, true]);
+            ns.tprintf(`${c.cyan}pserv-${i}: Starting ${master} ${nram} true${c.reset}`);
+            let pid = ns.exec(master, `pserv-${i}`, 1, ...[nram, true]);
 
             if (pid == 0)
                ns.tprintf(`${c.red}Could not start the looper master.${c.reset}`);
          }
          else
-            ns.tprintf(`${c.magenta}No root access to ${target}, so not starting looper master !!!${c.reset}`)
-      }
+            ns.tprintf(`${c.magenta}No root access to ${nram}, so not starting looper master !!!${c.reset}`)
+         
+         i++;
+      });
    }
 }
