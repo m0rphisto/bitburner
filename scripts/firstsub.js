cd0b3670f9d921f6a797c678b6aee87416ee381e
m0rphisto <warpfactor.net@gmail.com>
2023-08-15 07:52:52 +0200

    Bugfix: hasRootAccess() ???

--- scripts/firstsub.js
+++ scripts/firstsub.js
@@ -1,5 +1,5 @@
 /** 
- * $Id: firstsub.js v0.2 2023-08-14 21:27:30 CEST 4.80GB .m0rph $
+ * $Id: firstsub.js v0.3 2023-08-15 07:51:37 CEST 4.80GB .m0rph $
  * 
  * description:
  *    After augmentation installation we have to start at the beginning,
@@ -47,15 +47,19 @@ export async function main(ns) {
    //scanned.forEach(a => ns.scan(a).forEach(b => scanned.add(b).delete('home')));
    for (let i = 0; i < hosts[ports].length; i++)
    {
-      let host = hosts[ports][i];
+      let cmd, host = hosts[ports][i];
       
-      if (ns.fileExists('BruteSSH.exe'))  ns.brutessh(host);
-      if (ns.fileExists('FTPCrack.exe'))  ns.ftpcrack(host);
-      if (ns.fileExists('HTTPWorm.exe'))  ns.httpworm(host);
-      if (ns.fileExists('relaySMTP.exe')) ns.relaysmtp(host);
-      if (ns.fileExists('SQLInject.exe')) ns.sqlinject(host);
+      if (!ns.hasRootAccess(host))
+      {
+         if (ns.fileExists('BruteSSH.exe'))  ns.brutessh(host);
+         if (ns.fileExists('FTPCrack.exe'))  ns.ftpcrack(host);
+         if (ns.fileExists('HTTPWorm.exe'))  ns.httpworm(host);
+         if (ns.fileExists('relaySMTP.exe')) ns.relaysmtp(host);
+         if (ns.fileExists('SQLInject.exe')) ns.sqlinject(host);
 
-      ns.nuke(host);
+         ns.nuke(host);
+      }
+
       if (ns.hasRootAccess(host))
       {
          // Any further actions only make sense, if we 0wned the box.
@@ -82,26 +86,26 @@ export async function main(ns) {
             // Only hosts in home's subnet are directly connectable.
 
             case 0:
-               host = host == 'nectar-net'    ? 'sigma-cosmetics; connect nectar-net'   : host;
+               cmd = host == 'nectar-net'    ? 'sigma-cosmetics; connect nectar-net'   : host;
                break;
             case 1:
-               host = host == 'max-hardware'  ? 'sigma-cosmetics; connect max-hardware' : host;
-               host = host == 'zer0'          ? 'hong-fang-tea; connect zer0'           : host;
-               host = host == 'CSEC'          ? 'iron-gym; connect CSEC'                : host;
-               host = host == 'neo-net'       ? 'CSEC; connect neo-net'                 : host;
+               cmd = host == 'max-hardware'  ? 'sigma-cosmetics; connect max-hardware' : host;
+               cmd = host == 'zer0'          ? 'hong-fang-tea; connect zer0'           : host;
+               cmd = host == 'CSEC'          ? 'iron-gym; connect CSEC'                : host;
+               cmd = host == 'neo-net'       ? 'CSEC; connect neo-net'                 : host;
                break;
             case 2:
-               host = host == 'silver-helix'  ? 'max-hardware; connect silver-helix'    : host;
-               host = host == 'phantasy'      ? 'phantasy; connect neo-net'             : host;
-               host = host == 'omega-net'     ? 'nectar-net; connect omega-net'         : host;
-               host = host == 'avmnite-02h'   ? 'omega-net; connect avmnite-02h'        : host;
-               host = host == 'johnson-ortho' ? 'neo-net; connect johnson-ortho'        : host;
-               host = host == 'the-hub'       ? 'silver-helix; connect the-hub'         : host;
-               host = host == 'crush-fitness' ? 'neo-net; connect crush-fitness'        : host;
+               cmd = host == 'silver-helix'  ? 'max-hardware; connect silver-helix'    : host;
+               cmd = host == 'phantasy'      ? 'phantasy; connect neo-net'             : host;
+               cmd = host == 'omega-net'     ? 'nectar-net; connect omega-net'         : host;
+               cmd = host == 'avmnite-02h'   ? 'omega-net; connect avmnite-02h'        : host;
+               cmd = host == 'johnson-ortho' ? 'neo-net; connect johnson-ortho'        : host;
+               cmd = host == 'the-hub'       ? 'silver-helix; connect the-hub'         : host;
+               cmd = host == 'crush-fitness' ? 'neo-net; connect crush-fitness'        : host;
                break;
          }
 
-         shell(`home; connect ${host}; backdoor`);
+         shell(`home; connect ${cmd}; backdoor`);
          await ns.sleep(6000);
 
          ns.tprintf(`${c.magenta}And finaly installed the backdoor on ${host}.${c.reset}`);
