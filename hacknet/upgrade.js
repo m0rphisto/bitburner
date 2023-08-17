7cfca7af914b4673c4364934863029af707fb42d
m0rphisto <warpfactor.net@gmail.com>
2023-08-18 01:29:22 +0200

    Sleep timing adjusted.

--- hacknet/upgrade.js
+++ hacknet/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * $Id: upgrade.js v0.1 2023-08-17 08:13:35 6.70GB .m0rph $
+ * $Id: upgrade.js v0.1 2023-08-18 01:28:35 6.70GB .m0rph $
  * 
  * description:
  *    Automated hacknet node upgrade process.
@@ -19,17 +19,27 @@ import {d} from '/modules/datetime.js';
  * @param {string}   mode  File open mode >> w = create new file, a = append to file.
  */
 const timestamp = d.timestamp();
-function log (data, mode) {
+function log (ns, data, mode) {
    const m = mode ? mode : 'w';
    ns.print(`${c.cyan}${data}${c.reset}`);
    ns.write(`/log/hacknet.upgrade.${timestamp}.js`, data, m);
 }
 
+const sleep_time = (money) => {
+   switch (money)
+   {
+      case money < 10e6:  return 6e4; // 1   min
+      case money > 10e6:  return 3e4; // 30  sec
+      case money > 100e6: return 1e4; // 10  sec
+      case money > 1e9:   return 1e3; // 1   sec
+      case money > 100e9: return 500; // 0.5 sec 
+   }
+};
 const max = {
    nodes: 30,
-   Level: 200, 
-   Ram:   64,
-   Core:  16
+   level: 200, 
+   ram:   64,
+   cores:  16
 };
 async function upgrade(ns, type)
 {
@@ -40,19 +50,36 @@ async function upgrade(ns, type)
       // For interval calculation see quacksouls comments on is_upgrade_core_ram_cache():
       // https://github.com/quacksouls/bitburner/blob/main/src/hnet.js
       let
-         interval = ns.hacknet.getNodeStats(i).level % 30,
-         is_time  = interval === 0 ? !!1 : !!0;
+         is_money = ns.getServerMoneyAvailable('home'),
+         stats    = ns.hacknet.getNodeStats(i),
+         interval = stats.level  % 30;
 
-      if (
-         ns.getServerMoneyAvailable('home') > ns.hacknet[`get${type}UpgradeCost`](i, 1) &&
-         ns.hacknet.getNodeStats(i)[type] < max[type] && 
-         is_time
-      ) {
-         if (ns.hacknet[`upgrade${type}`](i)) log(`[${d.gettime()}] ${type} upgrade - node ${id}.`, 'a');
+      const is_time = {
+         'level' () { return !!1            },
+         'ram'   () { return interval === 0 },
+         'cores' () { return interval === 0 }
+      };
+      const cost = {
+         'level' (i) { return ns.hacknet.getLevelUpgradeCost(i) },
+         'ram'   (i) { return ns.hacknet.getRamUpgradeCost(i)   },
+         'cores' (i) { return ns.hacknet.getCoreUpgradeCost(i)  }
+      };
+      const upgrade = {
+         'level' (i) { return ns.hacknet.upgradeLevel(i) },
+         'ram'   (i) { return ns.hacknet.upgradeRam(i)   },
+         'cores' (i) { return ns.hacknet.upgradeCore(i)  }
+      };
+      //log(ns, `[${d.gettime()}] DEBUG - cost[${type}](i) - ${cost[type](i)}\n`, 'a');
+      //log(ns, `[${d.gettime()}] DEBUG - stats[${type}] - `+stats[type]+` :: max[type] - ${max[type]}\n`, 'a');
+      //log(ns, `[${d.gettime()}] DEBUG - is_time = ${is_time[type]()}\n`, 'a');
+
+      if (is_money > cost[type](i) && stats[type] < max[type] &&  (stats.level == max.level || is_time[type]()))
+      {
+         if (upgrade[type](i)) log(ns, `[${d.gettime()}] ${type} upgrade - node ${i}.\n`, 'a');
          else
             throw new Error(`Could not upgrade ${type} for hacknet node ${i}`);
 
-         await ns.sleep(6e4)
+         await ns.sleep(sleep_time(is_money));
       }
    }
 }
@@ -60,8 +87,9 @@ async function upgrade(ns, type)
 export async function main(ns) {
 
    'use strict';
-
-   ['getServerMoneyAvailable', 'sleep'].forEach(nl => ns.disableLog(nl));
+   
+   ns.disableLog('sleep');
+   ns.disableLog('getServerMoneyAvailable');
 
    let run = true;
    const chk = (type) => {
@@ -70,7 +98,8 @@ export async function main(ns) {
 
       for (let i = 0; i < max.nodes; i++)
       {
-         nodes += ns.hacknet.getNodeStats(i)[type] == max[type] ? 1 : 0;
+         const stats = ns.hacknet.getNodeStats(i);
+         nodes += stats[type] == max[type] ? 1 : 0;
       }
 
       return nodes == max.nodes ? !!1 : !!0;
@@ -79,13 +108,13 @@ export async function main(ns) {
    while (run)
    {
       // Check if we're done and can quit.
-      run = chk('Level') && chk('Ram') && chk('Core') ? !!0 : !!1;
+      run = chk('level') && chk('ram') && chk('cores') ? !!0 : !!1;
 
       // Upgrade nodes.
-      await upgrade('Level');
-      await upgrade('Ram');
-      await upgrade('Core');
+      await upgrade(ns, 'level');
+      await upgrade(ns, 'ram');
+      await upgrade(ns, 'cores');
 
-      await ns.sleep(6e4);
+      //await ns.sleep(6e4);
    }
 }
