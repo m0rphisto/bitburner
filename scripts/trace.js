21bbdd39351ce1be89d64e0fb82f21635cf2987f
m0rphisto <warpfactor.net@gmail.com>
2023-08-28 22:00:57 +0200

    Added a unix traceroute.

---
+++ scripts/trace.js
@@ -0,0 +1,14 @@
+/**
+ * $Id: trace.js v0.1 2023-08-28 21:36:41 3.30GB .m0rph $
+ * 
+ * @param {NS} ns
+ */
+import {exit, trace} from '/modules/helpers.js';
+import {has_count} from '/modules/arguments.js';
+export function autocomplete(data, args) { return [...data.servers] }
+export async function main(ns) {
+   has_count(ns.args, 0) && exit(ns, 'No argument passed.');
+   ns.serverExists(ns.args[0]) || exit(ns, 'Server does not exist.');
+   let [found, path] = trace(ns, 'home', '', ns.args[0]);
+   for (let host of path) ns.tprintf(host);
+}
