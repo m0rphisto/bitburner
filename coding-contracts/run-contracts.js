44d8f07fedd79593ac47937509afe71bd7178bbd
m0rphisto <warpfactor.net@gmail.com>
2023-09-06 16:28:14 +0200

    Added utilizer and library.

---
+++ coding-contracts/run-contracts.js
@@ -0,0 +1,41 @@
+/**
+ * $Id: run-contracts.js v0.1 2023-09-06 08:21:47 8.00GB .m0rph $
+ * 
+ * Description:
+ *  This scripts runs over the complete server network, searches for
+ *  coding contracts and solves them.
+ * 
+ * 
+ * @param {NS} ns
+ */
+import { c } from '/modules/colors.js';
+import { header, div, footer } from '/modules/helpers.js';
+import { ctypes, get_network } from '/lib/coding-contracts.js';
+export async function main(ns) {
+
+   const
+      net  = get_network(ns),
+      OK   = `${c.green}OK${c.white}`,
+      FAIL = `${c.red}FAIL${c.white}`;
+
+   header(ns, 'cc', 'run-contracts.js');
+
+   for (let host of net) {
+      const cc = {}, files = ns.ls(host, '.cct');
+      ns.tprintf(`${c.white}Searching coding contracts on ${host} ... ${files.length > 0 ? OK : FAIL}.`)
+      if (files.length < 1) continue;
+      for (const file of files) {
+         const ctype = ns.codingcontract.getContractType(file, host);
+         if (Object.keys(ctypes).includes(ctype)) {
+            cc.file = file;
+            cc.host = host;
+            ns.tprintf(`${c.white}Found: ${ctype}. Solving ...`)
+            await ns.run(ctypes[ctype], 1, JSON.stringify(cc));
+         } else {
+            ns.tprintf(`${c.magenta}Sorry, "${ctype}" not yet implemented.`)
+         }
+      }
+      div(ns);
+   }
+   footer(ns, 'Coding contracts run finished');
+}
--- coding-contracts/unique-paths.js
+++ coding-contracts/unique-paths.js
@@ -1,5 +1,5 @@
 /**
- * $Id: unique-paths.js v0.2 2023-09-06 03:37:15 3.00GB .m0rph $
+ * $Id: unique-paths.js v0.3 2023-09-06 16:20:08 18.00GB .m0rph $
  * 
  * description:
  *  Coding contract: Unique Paths in a Grid I.
@@ -9,29 +9,31 @@
  * 
  * @param {NS} ns 
  */ 
+import { c } from '/modules/colors.js';
 import { has_count } from '/modules/arguments.js';
 import { exit } from '/modules/helpers.js';
 export async function main(ns) {
-    has_count(ns.args, 0) && exit(ns, 'unique-paths: Run this script via run-contracts.js.')
-    const
-       cc = JSON.parse(ns.args[0]),
-       cdata = ns.codingcontract.getData(cc.file),
-       get_paths = (rows, cols) => {
-        let i, k, grid = [[]];
-        // Adding the first row of 1's to the array.
-        for (k = 0; k < cols; k++) {
+   has_count(ns.args, 0) && exit(ns, 'unique-paths: Run this script via run-contracts.js.')
+   const
+      cc = JSON.parse(ns.args[0]),
+      cdata = ns.codingcontract.getData(cc.file, cc.host),
+      get_paths = (rows, cols) => {
+         let i, k, grid = [[]];
+         // Adding the first row of 1's to the array.
+         for (k = 0; k < cols; k++) {
             grid[0].push(1);
-        }
-        // Iterate over each row.
-        for (i = 1; i < rows; i++) {
+         }
+         // Iterate over each row.
+         for (i = 1; i < rows; i++) {
             grid.push([1]); // Adding 1 to the first, left most square.
             // Get the total for the current square.
             for (k = 1; k < cols; k++) {
-                grid[i][k] = grid[i][k - 1] + grid[i - 1][k];
-            }
-        }
-        // Return the bottom right hand value that has the total.
-        return grid[i - 1][k - 1];
-    }
-    ns.codingcontract.attempt(get_paths(cdata[0], cdata[1]), cc.file, cc.host);
+            grid[i][k] = grid[i][k - 1] + grid[i - 1][k];
+         }
+      }
+      // Return the bottom right hand value that has the total.
+      return grid[i - 1][k - 1];
+   }
+   let result = ns.codingcontract.attempt(get_paths(cdata[0], cdata[1]), cc.file, cc.host);
+   ns.tprintf(`${c.white}Result: ${c.green}${result}`);
 }
