091605b089c055db9317a1c1f52a26c3eabcfa5c
m0rphisto <warpfactor.net@gmail.com>
2023-09-09 20:03:51 +0200

    Added comments ...

--- coding-contracts/total-ways-to-sum.js
+++ coding-contracts/total-ways-to-sum.js
@@ -22,16 +22,19 @@ export async function main(ns) {
          // Adding the first element to the array.
          ways[0] = 1;
          // Iterate over the range 1 to ((sum - 1) + 1) => sum.
-         for (let row = 1; row < sum; row++) {
-            // Iterate over the range 1 to (sum + 1).
-            for (let col = 1; col < sum + 1; col++)
-               if (col >= row)
-                  ways[col] = ways[col] + ways[col - row];
+         // example:
+         //      sum = 4  :=  for_loop_1(i < range + 1) ==> i <  sum
+         //    range = 3  :=  for_loop_2(k < sum   + 1) ==> k <= sum
+         for (let i = 1; i < sum; i++) {
+            // Iterate over the range 1 to sum.
+            for (let k = 1; k <= sum; k++)
+               if (k >= i)
+                  ways[k] = ways[k] + ways[k - i];
          }
          // Return the total number of ways to sum.
          return ways[sum];
       };
-      
+ 
    let result = await ns.codingcontract.attempt(get_ways(cdata), cc.file, cc.host);
    ns.tprintf(`${c.white}Result: ${c.green}${result}`);
 }
